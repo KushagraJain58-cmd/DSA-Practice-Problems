a811fe21700588d47a1f96627fe8b3eab28231ea
Kushagra Jain <81035005+KushagraJain58-cmd@users.noreply.github.com>
2022-02-22 22:43:22 +0530

    Update search-insert-position.cpp

--- Arrays/search-insert-position.cpp
+++ Arrays/search-insert-position.cpp
@@ -15,3 +15,25 @@ Example 2:
   Input: nums = [1,3,5,6], target = 2
   Output: 1
 */
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        
+        int idx=-1;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]==target || nums[i]>target)
+            {
+                idx=i;
+                break;
+            }
+            if(i==nums.size()-1 && nums[i]!=target)
+            {
+                idx=i+1;
+                break;
+            }
+        }
+        
+        return idx;
+    }
+};
