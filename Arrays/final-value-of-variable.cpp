7c3e1dca3546b553fbab2ce879eff02abb7a261d
Kushagra Jain <81035005+KushagraJain58-cmd@users.noreply.github.com>
2022-02-26 13:29:29 +0530

    Update final-value-of-variable.cpp

--- Arrays/final-value-of-variable.cpp
+++ Arrays/final-value-of-variable.cpp
@@ -27,3 +27,24 @@ Example 2:
   ++X: X is incremented by 1, X = 1 + 1 = 2.
   X++: X is incremented by 1, X = 2 + 1 = 3.
 */
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+        
+        int x=0;
+        
+        for(int i=0;i<operations.size();i++)
+        {
+            if(operations[i]=="--X" || operations[i]=="X--")
+            {
+                x-=1;
+            }
+            if(operations[i]=="++X" || operations[i]=="X++")
+            {
+                x+=1;
+            }
+        }
+        
+        return x;
+    }
+};
