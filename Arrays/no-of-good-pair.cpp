7e1cd4fda032766f86a4baefa19fece1829df715
Kushagra Jain <81035005+KushagraJain58-cmd@users.noreply.github.com>
2022-03-01 18:03:08 +0530

    Update no-of-good-pair.cpp

--- Arrays/no-of-good-pair.cpp
+++ Arrays/no-of-good-pair.cpp
@@ -16,3 +16,23 @@ Example 2:
   Output: 6
   Explanation: Each pair in the array are good.
 */
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) {
+        
+        int count=0;
+        
+        for(int i=0;i<nums.size()-1;i++)
+        {
+            for(int j=i+1;j<nums.size();j++)
+            {
+                if(nums[i]==nums[j] && i<j)
+                {
+                    count++;
+                }
+            }
+        }
+        
+        return count;
+    }
+};
