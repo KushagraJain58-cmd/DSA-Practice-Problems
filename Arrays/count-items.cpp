6eead264c09ca20f89cd0056ca6eb5373375549c
Kushagra Jain <81035005+KushagraJain58-cmd@users.noreply.github.com>
2022-03-05 23:24:25 +0530

    Update count-items.cpp

--- Arrays/count-items.cpp
+++ Arrays/count-items.cpp
@@ -16,3 +16,37 @@ Example 1:
   Output: 1
   Explanation: There is only one item matching the given rule, which is ["computer","silver","lenovo"].
 */
+class Solution {
+public:
+    int countMatches(vector<vector<string>>& items, string ruleKey, string ruleValue) {
+     
+        int count=0;
+        
+        for(int i=0;i<items.size();i++)
+        {
+                if(ruleKey=="type")
+                {
+                    if(items[i][0]==ruleValue)
+                    {
+                        count++;
+                    }
+                }
+                if(ruleKey=="color")
+                {
+                    if(items[i][1]==ruleValue)
+                    {
+                        count++;
+                    }
+                }
+                if(ruleKey=="name")
+                {
+                    if(items[i][2]==ruleValue)
+                    {
+                        count++;
+                    }
+                }
+        }
+        
+        return count;
+    }
+};
