0d761ebb8bb3fb69212332e66de73acaf37a4f69
Kushagra Jain <81035005+KushagraJain58-cmd@users.noreply.github.com>
2022-03-19 01:44:31 +0530

    Update and rename check-if-two-strings to check-if-two-strings.cpp

--- Arrays/check-if-two-strings.cpp
+++ Arrays/check-if-two-strings.cpp
@@ -14,3 +14,30 @@ Example 1:
   word2 represents string "a" + "bc" -> "abc"
   The strings are the same, so return true.
 */
+class Solution {
+public:
+    bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
+        
+        string a="";
+        string b="";
+        
+        for(int i=0;i<word1.size();i++)
+        {
+            a+=word1[i];
+        }
+        for(int j=0;j<word2.size();j++)
+        {
+            b+=word2[j];
+        }
+        
+        if(a==b)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+        
+    }
+};
