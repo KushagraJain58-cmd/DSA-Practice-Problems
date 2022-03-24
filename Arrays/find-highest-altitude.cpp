b8b80872a46bb0fdf5d62f5c2534a36e85257720
Kushagra Jain <81035005+KushagraJain58-cmd@users.noreply.github.com>
2022-03-24 22:11:39 +0530

    Update find-highest-altitude.cpp

--- Arrays/find-highest-altitude.cpp
+++ Arrays/find-highest-altitude.cpp
@@ -11,3 +11,22 @@ Example 1:
   Output: 1
   Explanation: The altitudes are [0,-5,-4,1,1,-6]. The highest is 1.
 */
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        
+        int alt[gain.size()+1];
+        int maxalt=0;
+        alt[0]=0;
+        for(int i=0;i<gain.size();i++)
+        {
+            alt[i+1]=gain[i]+alt[i];
+        }
+        for(int i=0;i<gain.size()+1;i++)
+        {
+            maxalt=max(maxalt,alt[i]);
+        }
+        
+        return maxalt;
+    }
+};
