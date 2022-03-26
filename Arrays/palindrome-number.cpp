a4f524b1ab5eaf691f9af4c735026483b5f0bfb4
Kushagra Jain <81035005+KushagraJain58-cmd@users.noreply.github.com>
2022-03-26 15:03:15 +0530

    Update palindrome-number.cpp

--- Arrays/palindrome-number.cpp
+++ Arrays/palindrome-number.cpp
@@ -12,3 +12,43 @@ Example 1:
   Output: true
   Explanation: 121 reads as 121 from left to right and from right to left.
 */
+class Solution {
+public:
+    bool isPalindrome(int x) {
+       
+        if(x<0)
+        {
+            return false;
+        }
+        vector<int> rev;
+        while(x)
+        {
+            rev.push_back(x%10);
+            x=x/10;
+        }
+        int i=0;
+        int j=rev.size()-1;
+        
+        while(i<j)
+        {
+            if(rev[i]!=rev[j])
+            {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+        
+        // int rev=0;
+        // while(x>0)
+        // {
+        //     int lastdigit=x%10;
+        //     rev=rev*10+lastdigit;
+        //     x=x/10;
+        // }
+        // if(rev==x)
+        //     return true;
+        // return false;
+    }
+};
