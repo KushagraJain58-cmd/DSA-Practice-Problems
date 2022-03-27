d0f84c238af1f36d46d9973f4a3136c81a9de97e
Kushagra Jain <81035005+KushagraJain58-cmd@users.noreply.github.com>
2022-03-27 22:52:33 +0530

    Update max-product-of-two-ele.cpp

--- Arrays/max-product-of-two-ele.cpp
+++ Arrays/max-product-of-two-ele.cpp
@@ -12,3 +12,39 @@ Example 1:
   Explanation: If you choose the indices i=1 and j=2 (indexed from 0), you will get the maximum value, 
   that is, (nums[1]-1)*(nums[2]-1) = (4-1)*(5-1) = 3*4 = 12. 
 */
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        
+        int maxpro=0;
+        for(int i=0;i<nums.size()-1;i++)
+        {
+            for(int j=i+1;j<nums.size();j++)
+            {
+                int pro=0;
+                pro=(nums[i]-1)*(nums[j]-1);
+                maxpro=max(maxpro,pro);
+            }
+        }
+        
+        return maxpro;
+    }
+};
+
+// class Solution {                         0ms 100%
+//     public int maxProduct(int[] nums) {
+//         int max1 = Integer.MIN_VALUE;
+//         int max2 = max1;
+        
+//         for(int i =0; i<nums.length;i++){
+//             if(max1<nums[i]){
+//                 max2 = max1;
+//                 max1 = nums[i];
+//             } else if(max2<nums[i]){
+//                 max2=nums[i];
+//             }
+//         }
+        
+//         return (max1-1)*(max2-1);
+//     }
+// }
