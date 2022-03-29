b89428e8c34c37cd160626c18d603dad6a6d95f8
Kushagra Jain <81035005+KushagraJain58-cmd@users.noreply.github.com>
2022-03-29 22:39:41 +0530

    Update number-of-students-doing-homework.cpp

--- Arrays/number-of-students-doing-homework.cpp
+++ Arrays/number-of-students-doing-homework.cpp
@@ -15,3 +15,19 @@ Example 1:
   The second student started doing homework at time 2 and finished at time 2 and also wasn't doing anything at time 4.
   The third student started doing homework at time 3 and finished at time 7 and was the only student doing homework at time 4.
 */
+class Solution {
+public:
+    int busyStudent(vector<int>& startTime, vector<int>& endTime, int queryTime) {
+        
+       int count=0;
+    
+    for(int i=0;i<startTime.size();i++){
+        
+        if(startTime[i] <= queryTime && endTime[i] >=queryTime)
+            count++;
+        
+    }
+    return count;
+
+    }
+};
