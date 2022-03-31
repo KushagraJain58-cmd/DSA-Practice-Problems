c3f2f3d0d0bbb44808d5be789315c47dba5ae513
Kushagra Jain <81035005+KushagraJain58-cmd@users.noreply.github.com>
2022-03-31 21:27:40 +0530

    Update unique-integers-sum.cpp

--- Arrays/unique-integers-sum.cpp
+++ Arrays/unique-integers-sum.cpp
@@ -10,3 +10,22 @@ Example 1:
   Output: [-7,-1,1,3,4]
   Explanation: These arrays also are accepted [-5,-1,1,2,3] , [-3,-1,2,-2,4].
 */
+class Solution {
+public:
+    vector<int> sumZero(int n) {
+        
+        vector<int> arr;
+        
+        for(int i=1;i<=n/2;i++)
+        {
+            arr.push_back(i);
+            arr.push_back(-i);
+        }
+        if(n%2!=0)
+            {
+                arr.push_back(0);
+            }
+        
+        return arr;
+    }
+};
