8ff0fe524657caa368d6c48ef6a39d01ce2f0cbb
Kushagra Jain <81035005+KushagraJain58-cmd@users.noreply.github.com>
2022-03-31 21:27:15 +0530

    Create unique-integers-sum.cpp

---
+++ Arrays/unique-integers-sum.cpp
@@ -0,0 +1,12 @@
+/*                    LEETCODE PROBLEM: 1304
+                    Find N Unique Integers Sum up to Zero
+                      
+https://leetcode.com/problems/find-n-unique-integers-sum-up-to-zero/
+  
+Given an integer n, return any array containing n unique integers such that they add up to 0.
+
+Example 1:
+  Input: n = 5
+  Output: [-7,-1,1,3,4]
+  Explanation: These arrays also are accepted [-5,-1,1,2,3] , [-3,-1,2,-2,4].
+*/
