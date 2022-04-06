1f70f3d568b09581e822b8b4c7c31811dfa3c047
Kushagra Jain <81035005+KushagraJain58-cmd@users.noreply.github.com>
2022-04-06 23:59:23 +0530

    Update unique-integers-sum.cpp

--- Arrays/unique-integers-sum.cpp
+++ Arrays/unique-integers-sum.cpp
@@ -7,7 +7,7 @@ Given an integer n, return any array containin n uniqu integers such that they a
 
 Example 1:
   Input: n = 5
-  Output: [-7,-1,1,3,4]
+  Output: [-7,-1,1,3,4
   Explanation: These arrays also are accept [-5,-1,1,2,3] , [-3,-1,2,-2,4].
 */
 class Solution {
