66215dde3356dc9bae4c86089727a94e8aa3aa47
Kushagra Jain <81035005+KushagraJain58-cmd@users.noreply.github.com>
2022-04-09 02:57:58 +0530

    Update concatenation-of-array.cpp

--- Arrays/concatenation-of-array.cpp
+++ Arrays/concatenation-of-array.cpp
@@ -3,7 +3,7 @@
                    
 https://leetcode.com/problems/concatenation-of-array/  
   
-Given an integer array nums of length n, you want to create an array ans of length 2n where ans[i] == nums[i] and ans[i + n] == nums[i] for 0 <= i < n (0-indexed).
+Given an integer array nums of length n, you want to create an array ans of lengt 2n where ans[i] == nums[i] and ans[i + n] == nums[i] for 0 <= i < n (0-indexed).
 Specifically, ans is the concatenation of two nums arrays.
 Return the array ans.
 
