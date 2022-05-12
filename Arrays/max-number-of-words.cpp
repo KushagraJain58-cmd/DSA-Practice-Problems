45165ae74fe07ce9d26f65f055c162c9d67b3404
Kushagra Jain <81035005+KushagraJain58-cmd@users.noreply.github.com>
2022-05-13 03:20:41 +0530

    Update max-number-of-words.cpp

--- Arrays/max-number-of-words.cpp
+++ Arrays/max-number-of-words.cpp
@@ -8,7 +8,7 @@ You are given an array of strings sentences, where each sentences[i] represents 
 Return the maximum number of words that appear in a single sentence.
 
 Example 1:
-  Input: sentences = ["alice and bob love leetcode", "i think so too", "this is great thanks very much"]
+  Input: sentences = ["alice and bob love leetcode", "i think so too", "this is  great thanks very much"]
   Output: 6
   Explanation: 
   - The first sentence, "alice and bob love leetcode", has 5 words in total.
