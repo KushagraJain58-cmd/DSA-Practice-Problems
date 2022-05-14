6bdabf83f3f0c0abcc37e3af0a20589ff2555590
Kushagra Jain <81035005+KushagraJain58-cmd@users.noreply.github.com>
2022-05-14 15:31:49 +0530

    Update find-target-indices.cpp

--- Arrays/find-target-indices.cpp
+++ Arrays/find-target-indices.cpp
@@ -3,7 +3,7 @@
                    
 https://leetcode.com/problems/find-target-indices-after-sorting-array/  
   
-You are given a 0-indexed integer array nums and a target element  target.
+You are given a 0-indexed integer array nums and a target element target.
 A target index is an index i such that nums[i] == target.
 Return a list of the target indices of nums after sorting nums in non-decreasing order. If there are no target indices, return an empty list. The returned list must be sorted in increasing order.
 
