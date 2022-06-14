6562542a6d7d1a7e78d9b3c95b84880143b0b27f
Kushagra Jain <81035005+KushagraJain58-cmd@users.noreply.github.com>
2022-06-15 00:24:49 +0530

    Update find-first-palindromic-string.cpp

--- Arrays/find-first-palindromic-string.cpp
+++ Arrays/find-first-palindromic-string.cpp
@@ -3,7 +3,7 @@
                    
 https://leetcode.com/problems/find-first-palindromic-string-in-the-array/
   
-Given an array of strings words, return the first palindromic string in the array. If there is no such string, return an empty string "".
+Given an array of strings words, return the first palindromic  string in the array. If there is no such string, return an empty string "".
 A string is palindromic if it reads the same forward and backward.
 
 Example 1:
