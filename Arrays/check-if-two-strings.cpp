c020591304830f234cb93f632d94fbf8a2ff85f1
Kushagra Jain <81035005+KushagraJain58-cmd@users.noreply.github.com>
2022-06-23 01:22:46 +0530

    Update check-if-two-strings.cpp

--- Arrays/check-if-two-strings.cpp
+++ Arrays/check-if-two-strings.cpp
@@ -3,7 +3,7 @@
                    
 https://leetcode.com/problems/check-if-two-string-arrays-are-equivalent/
   
-Given two string arrays word1 and word2, r]eturn true if the two arrays represent the same string, and false otherwise.
+Given two string arrays word1 and word2, r]eturn true if the two  arrays represent the same string, and false otherwise.
 A string is represented by an array if the array elements concatenated in order forms the string.
 
 Example 1:
